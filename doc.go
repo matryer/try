9b7a7a1b0040e2f88e64205cd1c94c8dbe413a73
Mat Ryer <thatmatryer@gmail.com>
2015-02-19 17:25:41 -0800

    added example

--- doc.go
+++ doc.go
@@ -1,2 +1,11 @@
 // Package try provides retry functionality.
+//     var value string
+//     err := try.Do(func(attempt int) (error, bool) {
+//       var err error
+//       value, err = SomeFunction()
+//       return err, attempt < 5 // try 5 times
+//     })
+//     if err != nil {
+//       log.Fatalln("error:", err)
+//     }
 package try
